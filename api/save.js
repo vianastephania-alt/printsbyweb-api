8e937b82737b1465c9fd87478105126523a89797
vianastephania-alt <vianastephania@gmail.com>
2026-02-27 00:15:10 -0500

    Update redirect URL in save.js for POST requests

--- api/save.js
+++ api/save.js
@@ -6,7 +6,7 @@ export const config = {
 
 export default async function handler(req, res) {
   if (req.method === "POST") {
-    return res.redirect(302, "https://prinstbyweb.wuaze.com");
+    return res.redirect(302, "https://prinstbyweb.wuaze.com/PERSONALIZA-TU-PROPIO-DISE%C3%91O/");
   }
 
   return res.status(405).json({ error: "Método no permitido" });
