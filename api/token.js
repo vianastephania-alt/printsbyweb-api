6c7d50c7333e5f37881bc7af809b92db054cac5f
vianastephania-alt <vianastephania@gmail.com>
2026-02-26 21:49:46 -0500

    Update token.js

--- api/token.js
+++ api/token.js
@@ -1,4 +1,14 @@
 export default function handler(req, res) {
-  const token = "token-de-prueba-123";
-  res.status(200).json({ token });
+  const key = process.env.PIXLR_KEY;
+  const secret = process.env.PIXLR_SECRET;
+
+  if (!key || !secret) {
+    return res.status(500).json({ error: "Variables no cargadas" });
+  }
+
+  res.status(200).json({ 
+    message: "Variables cargadas correctamente",
+    keyLoaded: !!key,
+    secretLoaded: !!secret
+  });
 }
